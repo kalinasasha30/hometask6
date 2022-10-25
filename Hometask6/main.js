b72618b5413628351d02ae6107e453fec1743c63
kalinasasha30 <112653857+kalinasasha30@users.noreply.github.com>
2022-10-25 14:40:30 +0300

    Add files via upload

---
+++ Hometask6/main.js
@@ -0,0 +1,37 @@
+// №1
+
+// for (let i = 150; i >= 99 ; i--) {
+//     console.log(i)
+//     if (i == 99) {
+//         console.log('Stop')
+//         for (let a = 98; a >= 54; a--) {
+//             console.log(a)
+//         }
+//     }
+// }
+
+// №2
+
+// for (let i = 0; i < 51; i++) {
+//     console.log(i)
+// }
+
+// №3
+
+// for (let i = 11; i < 34; i++) {
+//     console.log(i)
+// }
+
+// №4
+
+// for (let i = 0; i < 101; i+=2) {
+//     console.log(i)
+// }
+
+// №5
+
+// let sum = 0
+// for (let i = 0; i < 101; i++) {
+//     sum += i
+// }
+// console.log(sum)
